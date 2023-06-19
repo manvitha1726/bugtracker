d303b721f5071289587d41e28fabee4b62902752
karthik5656 <Karthikyadav5656@gmail.com>
2023-06-19 11:48:12 +0530

    Revert "added some styling for icons and employee dropdown"
    This reverts commit b7dafca2e59521936bba9557e23f828e04082263.

--- src/Components/IssueForm.js
+++ src/Components/IssueForm.js
@@ -96,7 +96,7 @@ const initialFormData = {
       // if(response.payload == true|| response == true){
       //         NavigateToIssues();
       // }
-      NavigateToIssues();
+      // NavigateToIssues();
       // func(response);
     })
     .catch(error => {
@@ -193,7 +193,6 @@ const initialFormData = {
          
           <div className="col-75">
             <EmployeeDropdown val={val1} callBackFunc={setSelectedAssignedEmployee} />
-            <br /><br />
             <AddEmployee func={setVal1} projectId={projectId} />
             {/* {console.log("emp selected", selectedAssignedEmployee)} */}
           </div>
@@ -258,9 +257,8 @@ const initialFormData = {
           </div>
         </div>
 
-        <button type="submit" onClick={handleSubmit}>Add Issue</button> 
+        <button type="submit" onClick={handleSubmit}>Add Issue</button> &nbsp;&nbsp;
         </form>
-        &nbsp;&nbsp;
                <button onClick={NavigateBackClick1}>Cancel</button>
         </>
         );
